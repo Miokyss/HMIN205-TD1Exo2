fa7c5c5736616061d2ef9aa6614a350d84cd77cd
Tessy <minotessy@gmail.com>
2021-02-01 12:51:02 +0100

    Début

---
+++ app/src/main/java/td1/exo2/MainActivity.java
@@ -0,0 +1,17 @@
+package td1.exo2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
